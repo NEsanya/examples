a9d2e32388966d6d909a83097925f8043ad776fc
NEsanya <53403671+NEsanya@users.noreply.github.com>
2021-08-24 19:57:36 +0300

    Create core_config.js

---
+++ core_config.js
@@ -0,0 +1,10 @@
+const { ThreeJSMap, GuidesLoader, NeuroAd } = require("neuro-core/components")
+
+const config = {
+  components: [ThreeJSMap, GuidesLoader, NeuroAd],
+  streaming_id: 10,
+  device_started_at: "9:00",
+  device_off_on: "20:00"
+}
+
+module.exports = config
